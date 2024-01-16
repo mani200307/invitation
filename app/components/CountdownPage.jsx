c4ec808818d02ee3e578830e068c22a629558d34
mani200307 <mani89509@gmail.com>
2024-01-16 11:04:25 +0530

    Final push check

--- app/components/CountdownPage.jsx
+++ app/components/CountdownPage.jsx
@@ -41,7 +41,7 @@ const CountdownPage = () => {
 
         timerComponents.push(
             <div className='flex flex-col justify-center items-center gap-3'>
-                <span className='bg-[#2F3C7E] text-[#FFFFFF] text-xl font-medium flex items-center justify-center w-12 md:w-16 aspect-square'>
+                <span className='bg-[#4831D4] text-[#FFFFFF] text-xl font-medium flex items-center justify-center w-12 md:w-16 aspect-square'>
                     {timeLeft[interval]}
                 </span>
                 <span className='text-sm font-medium text-[#2F3C7E] text-center'>
@@ -52,7 +52,7 @@ const CountdownPage = () => {
     });
 
     return (
-        <div id='countdown' className='flex flex-col mt-4 py-16 justify-center gap-4 items-center'>
+        <div id='countdown' className='flex flex-col py-16 justify-center gap-4 items-center'>
             <h1 className='text-3xl text-[#2F3C7E] font-semibold mb-12'>Countdown</h1>
             <div className='flex gap-8'>
                 {timerComponents.length ? timerComponents : <span>Event started!</span>}
--- app/components/LocationPage.jsx
+++ app/components/LocationPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LocationPage = () => {
     return (
-        <div id='LocationPage' className='flex flex-col py-16 mb-4 justify-center gap-4 items-center'>
+        <div id='LocationPage' className='flex flex-col py-16 justify-center gap-4 items-center'>
             <h1 className='text-3xl text-[#2F3C7E] font-semibold mb-12'>Location</h1>
             <div className='flex flex-col w-fit bg-[#4831D4] shadow-lg rounded-lg border-[1px] p-4 items-center gap-12'>
                 <h1 className='text-xl text-[#FFFFFF] text-center'>Simbu Meera Mahal</h1>
